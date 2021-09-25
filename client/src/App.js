5d508290b1c63764c7dda1e062de00cc846ef81c
Ryely <ryely.moore@outlook.com>
2021-09-25 00:20:07 -0600

    created react app and set up initial link with the back end

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,29 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// reroutes the react app to localhost: 3001
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
         <Home />
       </div>
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
