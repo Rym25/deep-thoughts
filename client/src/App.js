4004fb1d18baf702c64367cc7c6ba52e3b3587a2
Ryely <ryely.moore@outlook.com>
2021-09-25 01:21:55 -0600

    created routes for Single Thoughts and User Profiles

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,14 @@ import Home from './pages/Home';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 // reroutes the react app to localhost: 3001
 const httpLink = createHttpLink({
   uri: '/graphql'
@@ -20,13 +28,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+            {/* sets what routes the SPA will be looking for in the URL */}
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile/:username?" component={Profile} />
+            <Route exact path="/thought/:id" component={SingleThought} />
+
+            <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
